a1daa6bafed5c4e606011e5527fed0c217940901
vanessaBonito <vanebonito@hotmail.com>
2022-10-27 16:06:32 -0500

    Add validations to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,16 @@
 class Post < ApplicationRecord
+  # Associations
   belongs_to :author, class_name: 'User'
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
 
+  # Validations
+  validates :title, presence: true
+  validates :title, length: { maximum: 250, too_long: 'Title must not exceed 250 characters' }
+  validates_numericality_of :comments_counter, only_integer: true, greater_than_or_equal: 0
+  validates_numericality_of :likes_counter, only_integer: true, greater_than_or_equal: 0
+
+  # Methods
   after_create :update_user_posts_counter
 
   private
