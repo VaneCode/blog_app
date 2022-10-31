481edd1b4b3bf770ac1e7da540b70fd3a0a8707d
vanessaBonito <vanebonito@hotmail.com>
2022-10-31 17:20:34 -0500

    Test responsive status

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,24 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /users' do
-    it 'returns http success' do
-      get '/posts/users'
-      expect(response).to have_http_status(:success)
+  context 'If response status was correct' do
+    describe 'GET /index' do
+      it 'returns http success' do
+        get '/users/:user_id/posts'
+        expect(response).to have_http_status(:success)
+      end
     end
-  end
 
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/posts/index'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/posts/show'
-      expect(response).to have_http_status(:success)
+    describe 'GET /show' do
+      it 'returns http success' do
+        get '/users/:user_id/posts/:id'
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
