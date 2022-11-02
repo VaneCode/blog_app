994f2f2292f21e97b4f375c3cf0a0bbcdb51f909
vanessaBonito <vanebonito@hotmail.com>
2022-11-02 14:48:03 -0500

    Create users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all.order(id: :asc)
   end
 
   def show
     @user = User.find(params[:id])
+    @posts = User.recent_posts
   end
 end
