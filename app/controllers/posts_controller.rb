3e0350bbc30688f83004e80e68e5f28696ece18b
vanessaBonito <vanebonito@hotmail.com>
2022-11-03 12:47:56 -0500

    Add method create in PostController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,17 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @author = current_user
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post.author = @author
+    if @post.save
+      flash[:success] = 'Post saved successfully.'
+      redirect_to user_path(@author.id)
+    else
+      flash.now[:error] = 'Error: Post could not be saved.'
+      render :new
+    end
+  end
 end
