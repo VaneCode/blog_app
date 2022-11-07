0a6f6f905f92857b6fe9f8bc479601b483188b69
unknown <tyaps002@gmail,com>
2022-11-07 21:16:59 +0100

    Add test for user name for the view

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe 'users/index.html.erb', type: :view do
         visit users_path
         @users = User.all
     end
+
+    it 'should see the username of all other users.' do
+      @users.each do |user|
+          expect(page).to have_content(user.name)
+      end
+    end
   end
 end
