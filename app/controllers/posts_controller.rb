50c729873990fff1e3fb9d77d0e25e51fe5c2bab
vanessaBonito <vanebonito@hotmail.com>
2022-11-09 17:43:33 -0500

    Add route destroy for posts and comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   load_and_authorize_resource
-
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes([:author]).order(created_at: :desc)
