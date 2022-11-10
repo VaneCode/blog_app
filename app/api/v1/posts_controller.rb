ae0d243f404f3e1f5b6997ffb39a86b5774b34c7
vanessaBonito <vanebonito@hotmail.com>
2022-11-10 17:21:50 -0500

    Add api endpoints to create and show comments

--- app/api/v1/posts_controller.rb
+++ app/api/v1/posts_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::PostsController < ApplicationController
-    def index
-      @posts = User.find(params[:user_id]).posts
-  
-      respond_to format do
-        format.json { render json: @posts }
-        format.html { render json: @posts }
-      end
+  def index
+    @posts = User.find(params[:user_id]).posts
+
+    if @posts
+      render json: { status: 'SUCCESS', message: 'Fetched all the posts successfully', data: @friends }, status: :ok
+    else
+      render json: @posts.errors, status: :bad_request
     end
   end
-  
+end
