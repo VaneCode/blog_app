47689f36daa8dd1daedb0e13372febf2cbf3dc7e
vanessaBonito <vanebonito@hotmail.com>
2022-11-11 15:22:20 -0500

    Add endpoint's routes in README.md

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::AuthenticationController < Api::V1::ApplicationController
-  # skip_before_action :authenticate_request
   # eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyLCJleHAiOjE2Njg3OTU0Nzl9.3WzbBxioksVmzVp6JgmH_yHTmiTY849VmU_HV5vvJOM
 
   def login
--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,4 @@
 class Api::V1::CommentsController < Api::V1::ApplicationController
-  # skip_before_action :verify_authenticity_token
-
   before_action :authenticate_request
 
   def index
